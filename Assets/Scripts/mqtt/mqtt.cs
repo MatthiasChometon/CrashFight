f2e3e1cdbe953f04b3f4295ae20e18b414e32663
MatthiasChometon <matthias.chometon@ynov.com>
2021-05-12 10:13:02 +0100

    feat mqtt subscribe

--- Assets/Scripts/mqtt/mqtt.cs
+++ Assets/Scripts/mqtt/mqtt.cs
@@ -27,24 +27,29 @@ public class mqtt : MonoBehaviour
         client.Connect(clientId);
 
         // subscribe to the topic "/home/temperature" with QoS 2 
-        client.Subscribe(new string[] { "/connect" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-
-
-
-
-
+        client.Subscribe(new string[] { "connect" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
 
     }
     void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
     {
 
-        Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+        //Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
 
-        if (e.Topic == "/connect")
+        if (e.Topic == "connect")
         {
+            Debug.Log("connect");
             connectToJoyStick(e);
         }
-        
+
+        if (e.Topic == "command/1")
+        {
+            Debug.Log("Received: 1 " + System.Text.Encoding.UTF8.GetString(e.Message));
+        }
+
+        if (e.Topic == "command/2")
+        {
+            Debug.Log("Received: 2 " + System.Text.Encoding.UTF8.GetString(e.Message));
+        }
 
     }
 
@@ -55,9 +60,10 @@ public class mqtt : MonoBehaviour
         int Id = PlayersId.Count;
 
         Debug.Log(Id);
+        Debug.Log("joystick/" + System.Text.Encoding.UTF8.GetString(e.Message));
 
         client.Publish("joystick/" + System.Text.Encoding.UTF8.GetString(e.Message), System.Text.Encoding.UTF8.GetBytes(Id.ToString()), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-
+        client.Subscribe(new string[] { "command/" + Id.ToString() }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
     }
 
     void OnGUI()
